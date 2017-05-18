0c0ea0a4fd696ad73b18e20393ba124cbc353472
Zachary Kohnen <DusterTheFirst@users.noreply.github.com>
2017-05-18 15:03:52 -0400

    af

---
+++ CommandHandler/CommandHandler/HelpCommands.js
@@ -0,0 +1,22 @@
+﻿var help = {
+    name: "help",
+    aliases: ["?"],
+    args: null,
+    description: "Get All Commands",
+    method: HelpCommand
+};
+var helpsearch = {
+    name: "help",
+    aliases: ["?"],
+    args: null,
+    description: "Get Command Info",
+    method: HelpSearchCommand
+};
+
+function HelpCommand(message, args) {
+
+}
+
+function HelpSearchCommand(message, args) {
+
+} 
--- CommandHandler/app.js
+++ CommandHandler/app.js
@@ -1,3 +1,19 @@
-'use strict';
+const Discord = require("discord.js");
+const settings = require('./setting.json');
+const Commands = require("./CommandHandler/CommandHandler.js");
 
-console.log('Hello world');
+const client = new Discord.Client();
+
+var CommandHandler = new Commands.CommandHandler();
+
+CommandHandler.prefix = "!";
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.username}!`);
+});
+
+client.on('message', msg => {
+    CommandHandler.Handle(msg);
+});
+
+client.login(settings.token);
