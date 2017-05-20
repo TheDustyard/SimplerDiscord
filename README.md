aebcc03fa61544dcd8fa0ca0b61cac7f78f9ad39
Zachary Kohnen <DusterTheFirst@users.noreply.github.com>
2017-05-20 07:45:00 -0400

    asdsadasdd

--- README.md
+++ README.md
@@ -1,4 +1,53 @@
-# Discord.js-Commands
-A better(unofficial) command handler for Discord.js
+# Installation
 
-VERY BETA DO NOT USE
+## Use NPM
+```npm install simpler-discord```
+
+# Usage
+
+#### Create a command handler
+```javascript
+const SimplerDiscord = require("Simpler-Discord");
+
+var Commands = new SimplerDiscord.CommandHandler("[command-prefix]");
+```
+
+#### Create A Command
+```js
+var PingCommand = new SimplerDiscord.Command("ping", null, "Ping the bot", Ping);
+var CombineCommand = new SimplerDiscord.Command("combine", ["first", "second"], "Combine the two strings", Combine);
+```
+The first value in the constructor is the command name,
+The second value is the arguments, set it as null for none, 
+The third value is the commands' description, and lastly
+The fourth value is the method to call
+
+```js
+function Ping(mesage, args, handler) {
+    message.channel.send("pong");
+}
+
+function Combine(mesage, args, handler) {
+    message.channel.send(args[0] + args[1]);
+}
+```
+The command methods should take 3 parameters,
+The message sent, 
+The arguments (if you specified them above),
+And the command handler that called it
+
+#### Regester Your Commands
+```js
+Commands.AddCommand(PingCommand, "Utility Commands");
+Commands.AddCommand(CombineCommand, "Utility Commands");
+```
+The first value is the command to add,
+The second value is the catagory it should be in in the help command (optional)
+
+#### Call Your Commands
+```js
+client.on('message', msg => {
+    Commands.Handle(msg);
+});
+```
+In your message event, add the handle method, and you will be done :)
--- SimplerDiscordExample/app.js
+++ SimplerDiscordExample/app.js
@@ -1,24 +1,23 @@
 const Discord = require("discord.js");
 const settings = require('./setting.json');
-const SimplerDiscord = require("./SimplerDiscord");
+const SimplerDiscord = require("Simpler-Discord");
 
 const client = new Discord.Client();
 
 var Commands = new SimplerDiscord.CommandHandler("!");
 
-Commands.AddCommand(new SimplerDiscord.Command("commandone", null, "a command", (message, args) => message.channel.send("COMMAND 1")), "caragory 1");
-Commands.AddCommand(new SimplerDiscord.Command("command2", ["an arg"], "a command with args", (message, args) => message.channel.send("COMMAND 2 WITH ONE ARG, " + args)), "catagory 2");
-Commands.AddCommand(new SimplerDiscord.Command("command2", ["one", "two"], "a command with the same name, but diferent args", (message, args) => message.channel.send("COMMAND 2 WITH 2 ARGS, " + args)), "catagory 3");
+var PingCommand = new SimplerDiscord.Command("ping", null, "Ping the bot", Ping);
+var CombineCommand = new SimplerDiscord.Command("combine", ["first", "second"], "Combine the two strings", Combine);
 
-Commands.AddCommand(new SimplerDiscord.Command("mention", ["name", "type"], "mention the given thing", MentionCommmand, "Util Commands"));
-Commands.AddCommand(new SimplerDiscord.Command("emoji", ["name"], "get an emoji", EmojiCommand, "Util Commands"));
+Commands.AddCommand(PingCommand, "Utility Commands");
+Commands.AddCommand(CombineCommand, "Utility Commands");
 
-function EmojiCommand(message, args) {
-    message.channel.send(new SimplerDiscord.Emoji(message.guild, args[0]).toString());
+function Ping(mesage, args, handler) {
+    message.channel.send("pong");
 }
 
-function MentionCommmand(message, args) {
-    message.channel.send(new SimplerDiscord.Mention(message.guild, args[0], args[1]).toString());
+function Combine(mesage, args, handler) {
+    message.channel.send(args[0] + args[1]);
 }
 
 client.on('ready', () => {
